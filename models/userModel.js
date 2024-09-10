50e65af0db4b4271e4e97ca908397789f803a527
Techtees <taiwojoshuaoye@gmail.com>
2024-09-10 16:05:24 +0100

    user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose")
-const { type } = require("os")
 
 const userSchema = mongoose.Schema({
   name: {
@@ -8,8 +7,24 @@ const userSchema = mongoose.Schema({
   },
   email: {
     type: String,
-    require: [true, "Please add your mail"]
+    require: [true, "Please add your mail"],
+    unique: true,
+    trim: true,
+    match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email'
+    ],
+  },
+  password: {
+    type: String,
+    require: true,
+    minLength: [6, "password must be up to 6 characters"]
+  },
+  isAdmin:{
+    type: Boolean,
+    default: false,
   }
+}, {
+    timestamps: true
 })
 
 const User = mongoose.model("User", userSchema)
