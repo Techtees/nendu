b906c2f5ecbedae0d28b093e45d118e1fa7801cb
Techtees <taiwojoshuaoye@gmail.com>
2024-09-10 16:05:52 +0100

    json webtoken

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const dotenv = require("dotenv").config()
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
+const userRoute = require("./routes/userRoute")
+const errorHandler = require("./middleware/errorMiddleware")
 
 // initialize our app
 const app = express()
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5005
 
 
 // middlewares
@@ -16,12 +18,15 @@ app.use(express.json())
 app.use(bodyParser.json())
 app.use(express.urlencoded({extended: false}))
 
-// create Route
+// Route middleware
+app.use("/api/users", userRoute)
 
+// create Route
 app.get("/", (req,res) => {
     res.send("home")
 })
 
+app.use(errorHandler)
 // connect to db and start server
 mongoose
 .connect(process.env.MONGO_URI)
@@ -31,12 +36,3 @@ mongoose
     })
 })
 .catch(err => console.log(err))
-
-// mongoose
-// .connect(process.env.MONGO_URI)
-// .then(() => {
-//     app.listen(PORT, () => {
-//         console.log(`Server running on ${PORT}`)
-//     })
-// })
-// .catch((err) => console.log(err))
