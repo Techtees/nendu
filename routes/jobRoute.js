b766f0e64a3dfa68491d93183a4186fbad34e6ed
Techtees <taiwojoshuaoye@gmail.com>
2024-09-20 20:44:42 +0100

    create function to update Jobs

--- routes/jobRoute.js
+++ routes/jobRoute.js
@@ -1,12 +1,15 @@
 
 
 const express = require('express')
-const { createJob, getJob } = require('../controllers/jobController')
+const { createJob, getJob, getJobs, deleteJob, updateJob } = require('../controllers/jobController')
 const router = express.Router()
 const protect = require('../middleware/authMiddleware.')
 
 
 router.post('/', protect, createJob)
-router.get('/', protect, getJob)
+router.get('/', protect, getJobs)
+router.get('/:id', protect, getJob)
+router.delete('/:id', protect, deleteJob)
+router.put('/:id', protect, updateJob)
 
 module.exports = router
